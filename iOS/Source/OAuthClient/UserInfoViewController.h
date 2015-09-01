02925e8f8a28add9c3105c98486046280ffbd5aa
jkurttogh <jesse.kurtto@ubisecure.com>
2015-09-01 12:41:29 +0300

    Unpacked source code

---
+++ iOS/Source/OAuthClient/UserInfoViewController.h
@@ -0,0 +1,16 @@
+//
+//  UserInfoViewController.h
+//  OAuthClient
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface UserInfoViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *emailLabel;
+@property NSString *name;
+@property NSString *email;
+
+@end
